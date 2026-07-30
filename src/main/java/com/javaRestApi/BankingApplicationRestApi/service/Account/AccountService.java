fd26fbbba6e8aca31a571c8b13d61bce94b6ff5a
Nitish Thakur <nitishrajput2604@gmail.com>
2026-07-30 21:03:02 +0530

    refeat - ReBuild Service Layer and improve business logic.

--- src/main/java/com/javaRestApi/BankingApplicationRestApi/service/Account/AccountService.java
+++ src/main/java/com/javaRestApi/BankingApplicationRestApi/service/Account/AccountService.java
@@ -1,120 +1,53 @@
 package com.javaRestApi.BankingApplicationRestApi.service.Account;
 
+import com.javaRestApi.BankingApplicationRestApi.Execption.UserNotFound;
 import com.javaRestApi.BankingApplicationRestApi.Model.AccountDTO.Account;
-import com.javaRestApi.BankingApplicationRestApi.Model.AccountDTO.AccountRequestDTO;
-import com.javaRestApi.BankingApplicationRestApi.Model.AccountDTO.AccountResponseDTO;
-
-import com.javaRestApi.BankingApplicationRestApi.Model.CustomerDTO.CustomerDto;
-import com.javaRestApi.BankingApplicationRestApi.Repository.AccountRepository;
-import com.javaRestApi.BankingApplicationRestApi.Repository.CustomerRepository;
+import com.javaRestApi.BankingApplicationRestApi.Repository.AccountRepositry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
 
 @Service
 public class AccountService {
-
     @Autowired
-    private AccountRepository accountRepository;
-
-    @Autowired
-    private CustomerRepository customerRepository;
-
-    // Create Account
-    public AccountResponseDTO createAccount(AccountRequestDTO request) {
-
-        CustomerDto customer = customerRepository.findById(Math.toIntExact(request.getCustomerId())).orElse(null);
-
-        if (customer == null) {
-            return null;
+    private AccountRepositry repositry;
+    public Account createAccount(Account account) {
+        String id = account.getAccountNumber();
+        Account account1 = repositry.findByAccountNumber(id);
+        if(account1 != null){
+           throw new UserNotFound("Please check the Account No,It must be unique: ");
+        }else {
+            return repositry.save(account);
         }
-
-        Account account = new Account();
-
-        account.setAccountType(request.getAccountType());
-        account.setBalance(request.getOpeningBalance());
-        account.setStatus("ACTIVE");
-
-        // Generate Random 10 Digit Account Number
-        Random random = new Random();
-        String accountNumber = String.valueOf(1000000000L + random.nextInt(900000000));
-
-        account.setAccountNumber(accountNumber);
-
-        // Set Customer Relation
-        account.setCustomer(customer);
-
-        Account savedAccount = accountRepository.save(account);
-
-        AccountResponseDTO response = new AccountResponseDTO();
-
-        response.setAccountId(savedAccount.getAccountId());
-        response.setAccountNumber(savedAccount.getAccountNumber());
-        response.setAccountType(savedAccount.getAccountType());
-        response.setBalance(savedAccount.getBalance());
-        response.setStatus(savedAccount.getStatus());
-
-        return response;
     }
 
-    // Get Single Account
-    public AccountResponseDTO getAccount(Long accountId) {
-
-        Account account = accountRepository.findById(accountId).orElse(null);
-
-        if (account == null) {
-            return null;
+    public Account getInfo(String accountNumber) {
+        Account account =repositry.findByAccountNumber(accountNumber);
+        if(account == null){
+            throw new UserNotFound("User not found check account number");
         }
-
-        AccountResponseDTO response = new AccountResponseDTO();
-
-        response.setAccountId(account.getAccountId());
-        response.setAccountNumber(account.getAccountNumber());
-        response.setAccountType(account.getAccountType());
-        response.setBalance(account.getBalance());
-        response.setStatus(account.getStatus());
-
-        return response;
+        return account;
     }
 
-    // Get Customer Accounts
-    public List<AccountResponseDTO> getCustomerAccounts(Long customerId) {
-
-        List<Account> accounts = accountRepository.findByCustomerCustomerId(customerId);
-
-        List<AccountResponseDTO> responseList = new ArrayList<>();
-
-        for (Account account : accounts) {
-
-            AccountResponseDTO response = new AccountResponseDTO();
-
-            response.setAccountId(account.getAccountId());
-            response.setAccountNumber(account.getAccountNumber());
-            response.setAccountType(account.getAccountType());
-            response.setBalance(account.getBalance());
-            response.setStatus(account.getStatus());
-
-            responseList.add(response);
+    public Account deleteAccount(String accountNumber) {
+        Account account =repositry.findByAccountNumber(accountNumber);
+        if(account == null){
+            throw new UserNotFound("User is not found to delete Check Account Number");
         }
-
-        return responseList;
+         repositry.delete(account);
+        return account;
     }
 
-    // Close Account
-    public void closeAccount(Long accountId) {
-
-        Account account = accountRepository.findById(accountId).orElse(null);
-
-        if (account != null) {
-
-            account.setStatus("CLOSED");
-
-            accountRepository.save(account);
+    public Account updateAccount(String accountNumber,Account accountObj) {
+        Account account = repositry.findByAccountNumber(accountNumber);
+        if(account == null){
+            throw new UserNotFound("NO Account Number Found Plz try again");
         }
 
-    }
+        account.setAccountType(accountObj.getAccountType());
+        account.setBalance(accountObj.getBalance());
+        account.setStatus(accountObj.getStatus());
+        return repositry.save(account);
 
+    }
 }
